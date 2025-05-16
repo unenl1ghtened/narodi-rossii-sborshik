2a25b41203f310b177897d9c8d481842f9d602a7
Margarita <margarita0work@gmail.com>
2025-05-16 13:53:46 +0300

    Added base styles

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,27 @@
-/* eslint-disable prettier/prettier */
-import { defineConfig } from "vite";
-import injectHTML from "vite-plugin-html-inject";
-import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import { defineConfig } from 'vite';
+import injectHTML from 'vite-plugin-html-inject';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        api: "modern-compiler",
+        api: 'modern-compiler',
       },
     },
   },
-  root: "./src",
-  base: "./",
+  root: './src',
+  base: './',
   server: {
     port: 3000,
     open: true,
     historyApiFallback: true,
   },
-  publicDir: "./src/assets",
+  publicDir: './src/assets',
   build: {
-    outDir: "../dist",
+    outDir: '../dist',
     emptyOutDir: true,
-    assetsDir: "assets",
+    assetsDir: 'assets',
     minify: false,
     terserOptions: {
       format: {
@@ -31,26 +30,26 @@ export default defineConfig({
     },
     rollupOptions: {
       input: {
-        main: "./src/index.html",
+        main: './src/index.html',
       },
       output: {
-        entryFileNames: "assets/js/[name].js", // Размещение JS в папке assets/js/
-        chunkFileNames: "assets/js/[name].js", // Размещение чанков в папке assets/js/
+        entryFileNames: 'assets/js/[name].js', // Размещение JS в папке assets/js/
+        chunkFileNames: 'assets/js/[name].js', // Размещение чанков в папке assets/js/
         assetFileNames: (assetInfo) => {
           if (/\.(png|jpg|jpeg|gif|svg)$/i.test(assetInfo.name)) {
-            return "assets/images/[name][extname]"; // Изображения в assets/images/
+            return 'assets/images/[name][extname]'; // Изображения в assets/images/
           }
           if (/\.(woff2?|ttf|otf|eot)$/i.test(assetInfo.name)) {
-            return "assets/fonts/[name][extname]"; // Шрифты в assets/fonts/
+            return 'assets/fonts/[name][extname]'; // Шрифты в assets/fonts/
           }
           if (/\.(css)$/i.test(assetInfo.name)) {
-            return "assets/css/[name][extname]"; // CSS в assets/css/
+            return 'assets/css/[name][extname]'; // CSS в assets/css/
           }
-          return "assets/[name][extname]"; // Для остальных файлов
+          return 'assets/[name][extname]'; // Для остальных файлов
         },
         manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return "vendor"; // Все сторонние модули будут в папке vendor
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Все сторонние модули будут в папке vendor
           }
         },
       },
