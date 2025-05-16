e8b174ab38f2afb438836ce07f5fb6e25d197aca
Margarita <margarita0work@gmail.com>
2025-05-16 17:23:45 +0300

    Created nations hero section

--- src/js/components/stickyHeader.js
+++ src/js/components/stickyHeader.js
@@ -2,12 +2,24 @@ export const useStickyHeader = () => {
   const header = document.querySelector('.header');
   if (!header) return;
 
-  const heroSection = document.querySelector('.hero');
   const headerHeight = header.offsetHeight;
+  const heroSection = document.querySelector('.hero');
+  const allSections = Array.from(document.querySelectorAll('section'));
 
-  if (heroSection) {
+  if (heroSection && allSections[0] === heroSection) {
+    // .hero существует и она первая — добавляем отступ ей
     heroSection.style.marginTop = `${headerHeight}px`;
   } else {
-    document.body.style.marginTop = `${headerHeight}px`;
+    // Ищем первую section (не header), которой можно задать отступ
+    const firstContentSection = allSections.find(
+      (section) => !section.classList.contains('header'),
+    );
+
+    if (firstContentSection) {
+      firstContentSection.style.marginTop = `${headerHeight}px`;
+    } else {
+      // На крайний случай — добавим отступ всему body
+      document.body.style.marginTop = `${headerHeight}px`;
+    }
   }
 };
---
+++ src/js/nations.js
@@ -0,0 +1,8 @@
+import '/scss/nations.scss';
+
+// components
+import { useBurger } from './components/burger';
+import { useStickyHeader } from './components/stickyHeader';
+
+useBurger();
+useStickyHeader();
