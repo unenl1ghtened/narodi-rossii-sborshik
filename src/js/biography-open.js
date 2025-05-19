c84153109df62708d4f052ac1385d6b369fb1c48
Margarita <margarita0work@gmail.com>
2025-05-19 10:34:39 +0300

    Created file structure for biography

---
+++ src/js/biography-open.js
@@ -0,0 +1,8 @@
+import '/scss/biography-open.scss';
+
+// components
+import { useBurger } from './components/burger';
+import { useStickyHeader } from './components/stickyHeader';
+
+useBurger();
+useStickyHeader();
