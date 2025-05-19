f7433435a1bb94cb01cbdff954ce205df3bd377a
Margarita <margarita0work@gmail.com>
2025-05-19 10:27:32 +0300

    Created file structure for tests

---
+++ src/js/tests-open.js
@@ -0,0 +1,8 @@
+import '/scss/tests-open.scss';
+
+// components
+import { useBurger } from './components/burger';
+import { useStickyHeader } from './components/stickyHeader';
+
+useBurger();
+useStickyHeader();
